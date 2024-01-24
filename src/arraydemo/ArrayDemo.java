252a7db00c656ec8733304d3ab614f255f6c1852
BrandonWainberg <bmw2629@gmail.com>
2024-01-24 13:59:38 -0500

    Update ArrayDemo.java

--- src/arraydemo/ArrayDemo.java
+++ src/arraydemo/ArrayDemo.java
@@ -14,7 +14,7 @@ public class ArrayDemo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        system.out.print("array");
         // TODO code application logic here
     }
     
